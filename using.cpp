23e1d5ac04d786c5dee7e88a50051f078edde734
SHIV SAGAR SHAH <71564583+ShivSagarShah@users.noreply.github.com>
2021-05-19 19:55:59 +0530

    Update using.cpp

--- using.cpp
+++ using.cpp
@@ -18,7 +18,7 @@ void multiply(int a[], int i, int &n){
 void factorial(int num){
     int a[1000]; //makes an array to store the answer's single digit in one box
     a[0] = 1; //first element should be 1
-    int n = 1;
+    int n = 1; //number of digit in the factorial
     for(int i = 2; i<=num; i++){ //start from 2 as multiply 1 with 1 gives 1 only
         multiply(a,i,n); //call this function each time you get a new number and n should be pass as refernce as it will be at the last position of array
     }
